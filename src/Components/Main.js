fe106b5a7380744ee91e15ce4e7d2f51b80daeae
veronvynguyen@gmail.com <veronvynguyen@gmail.com>
2018-12-24 07:58:25 -0800

    Built out header component

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,33 +4,40 @@ import AddPhoto from './AddPhoto'
 import {Route, Link} from 'react-router-dom'
 import Single from './Single'
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import Header from './Header/Header';
 
 class Main extends Component {
-
-    state = { loading: true }
+    state = { 
+        loading: true,
+    };
 
     componentDidMount() {
         this.props.startLoadingPost().then(() => {
             this.setState({loading: false})
         })
     }
-    render() {
 
+    render() {
+        const { openModal } = this.state;
         return (
         <Grid fluid>
+        <Header/>
              <Row className="show-grid">
                 <Col xs={12}>
-                    <h1>
+                    {/* <h1>
                         <Link to="/"> Asana Dog </Link>
-                    </h1>
+                    </h1> */}
+                    
                     <Route exact path="/" render={() => (
                         <div>
                             <PhotoWall {...this.props} />
                         </div>
                     )}/>
+
                     <Route path="/AddPhoto" render = {({history}) => (
-                                <AddPhoto {...this.props} onHistory={history}/>
-                            )}/>
+                        <AddPhoto {...this.props} onHistory={history}/>
+                    )}/>
+
                     <Route exact path="/single/:id" render = {(params) => (
                         <Single loading={this.state.loading} {...this.props} {...params}/> 
                     )}/>
--- src/Components/Single.js
+++ src/Components/Single.js
@@ -1,20 +1,19 @@
 import React, {Component} from 'react'
 import Photo from './Photo'
-import Comments from './Comments'
 
 class Single extends Component {
     render() {
         const {match, posts} = this.props
         const id = Number(match.params.id)
         const post = posts.find((post) => post.id === id)
-        const comments = this.props.comments[match.params.id] || []
         const index = this.props.posts.findIndex((post) => post.id === id)
         if (this.props.loading === true) {
             return <div className="loader"> ...loading </div>
         } else if (post) {
             return <div className='single-photo'>
                     <Photo post={post} {...this.props} index={index}/>
-                    <Comments startAddingComment={this.props.startAddingComment} comments={comments} id={id}/>
+                    <h3>{post.name}</h3>
+                    
                 </div>
         } else {
             return <h1> ...no post found </h1>
