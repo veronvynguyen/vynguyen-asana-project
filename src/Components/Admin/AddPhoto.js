862b09e04cf7153cff2fff653a07b567c35a84d3
veronvynguyen@gmail.com <veronvynguyen@gmail.com>
2018-12-25 18:37:10 -0800

    Updated layout for new post form

--- src/Components/Admin/AddPhoto.js
+++ src/Components/Admin/AddPhoto.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import FileUploader from "react-firebase-file-uploader";
-// import Illustration from '../images/Illustration.png';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { Picture } from 'react-responsive-picture';
+import Dog from '../../images/Dog.png';
+
 const uuid = require('uuid');
 
 class AddPhoto extends Component {
@@ -29,7 +30,7 @@ class AddPhoto extends Component {
     const weight = this.refs.weight.value;
     const gender = this.refs.gender.value;
     const breed = this.refs.breed.value;
-    const description = this.refs.description.value;
+    // const description = this.refs.description.value;
     const shelterAddress = this.refs.shelterAddress.value;
     const shelterPhone = this.refs.shelterPhone.value;
     const shelterEmail = this.refs.shelterEmail.value;
@@ -39,7 +40,7 @@ class AddPhoto extends Component {
       weight,
       gender,
       breed,
-      description, 
+      // description, 
       shelterAddress, 
       shelterPhone,
       shelterEmail
@@ -54,7 +55,7 @@ class AddPhoto extends Component {
         breed: this.state.breed,
         gender: this.state.gender,
         weight: this.state.weight,
-        shelterDescription: this.state.description,
+        // description: this.state.description,
         shelterAddress: this.state.shelterAddress,
         shelterPhone: this.state.shelterPhone,
         shelterEmail: this.state.shelterEmail,
@@ -75,15 +76,15 @@ class AddPhoto extends Component {
         this.setState({imageLink: url});
         const post = {
           id: Number(new Date()),
-          description: this.state.description,
-          name: this.state.name,
-          shelterAddress: this.state.shelterAddress,
-          shelterPhone: this.state.shelterPhone,
-          shelterEmail: this.state.shelterEmail,
           age: this.state.age,
           weight: this.state.weight,
           gender: this.state.gender,
           breed: this.state.breed,
+          // description: this.state.description,
+          name: this.state.name,
+          shelterAddress: this.state.shelterAddress,
+          shelterPhone: this.state.shelterPhone,
+          shelterEmail: this.state.shelterEmail,
           imageLink: url
         }
         this.props.startAddingPost(post);
@@ -96,13 +97,14 @@ class AddPhoto extends Component {
     return (
       <Grid fluid>
         <Row between="xs">
-          <Col xs={6} md={4}>
-           {/* <Picture src={Illustration} sizes="(max-width: 90%)"/>   */}
+          <Col xs={3}>
+          <Picture src={Dog} className="form-graphic"/>
           </Col>
-          <Col xs={6} md={8}>
-            <div className="form">
+          <Col xs={9}>
+            <div className="form"> 
+            <h1>Create a listing</h1>
               <form onSubmit={this.submitData}>
-                <label>Upload an image</label> 
+                <label>Upload photo:</label>
                 <FileUploader
                     accept="image/*"
                     name="avatar"
@@ -113,19 +115,28 @@ class AddPhoto extends Component {
                     onUploadSuccess={this.handleUploadSuccess}
                     onProgress={this.handleProgress}
                 />
+                <label for="name">Name</label>
+                <input type="text" ref="name" placeholder="Enter dog's name" onChange={this.handlePostChange}/>
+                <label for="age">Age</label>
+                <input type="text" ref="age" placeholder="Enter dog's age" onChange={this.handlePostChange}/>
+                <label for="weight">Weight</label>
+                <input type="text" ref="weight" placeholder="Enter dog's weight in lbs" onChange={this.handlePostChange}/>
+                <label for="gender">Gender</label>
+                <input type="text" ref="gender" placeholder="M/F" onChange={this.handlePostChange}/>
+                <label for="breed">Breed</label>
+                <input type="text" ref="breed" placeholder="Enter dod's breed" onChange={this.handlePostChange}/>
+                {/* <label for="description">Description</label>
+                <input type="text" ref="description" placeholder="Background (ex. dog's gender, weight, breed, age)." onChange={this.handlePostChange}/> */}
+                <label for="shelterAddress">Shelter's Address</label>
+                <input type="text" ref="shelterAddress" placeholder="Ex. 1211 Dogville, CA 29181" onChange={this.handlePostChange}/>
+                <label for="shelterPhone">Shelter's Phone</label>
+                <input type="text" ref="shelterPhone" placeholder="Ex. (714) 492-2912" onChange={this.handlePostChange}/>
+                <label for="shelterEmail">Shelter's Email</label>
+                <input type="text" ref="shelterEmail" placeholder="Ex. doglovers@example.com" onChange={this.handlePostChange}/>
 
-                <input type="text" ref="name" placeholder="Name" onChange={this.handlePostChange}/>
-                <input type="text" ref="age" placeholder="Age" onChange={this.handlePostChange}/>
-                <input type="text" ref="weight" placeholder="Weight" onChange={this.handlePostChange}/>
-                <input type="text" ref="gender" placeholder="Gender" onChange={this.handlePostChange}/>
-                <input type="text" ref="breed" placeholder="Breed" onChange={this.handlePostChange}/>
-                <input type="text" ref="description" placeholder="Background (ex. dog's gender, weight, breed, age)." onChange={this.handlePostChange}/>
-                <input type="text" ref="shelterAddress" placeholder="Shelter Address" onChange={this.handlePostChange}/>
-                <input type="text" ref="shelterPhone" placeholder="Shelter's Phone" onChange={this.handlePostChange}/>
-                <input type="text" ref="shelterEmail" placeholder="Shelter's Email" onChange={this.handlePostChange}/>
                 {this.state.isUploading && <p>Progress: {this.state.progress}%</p>}
                 {this.state.imageLink && <Picture src={this.state.imageLink} className="uploadImg"/>}
-                <input type="submit" />Submit
+                <input type="submit" />
               </form>
             </div>
           </Col> 
